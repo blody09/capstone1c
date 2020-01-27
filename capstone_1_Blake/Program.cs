c7b3273b8b31b14a93db18d5d252a6c8142c951b
blody09 <blakeody9@gmail.com>
2020-01-27 07:38:41 -0500

    capstone 1 upload

---
+++ capstone_1_Blake/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace capstone_1_Blake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userContinue = "y";
+
+
+            while (userContinue != "n")
+            {
+                Console.WriteLine("Please type a sentence you want to have translated to pig latin.");
+                string sentence = Console.ReadLine();
+                var pigLatin = GetSentenceInPigLatin(sentence);
+                Console.WriteLine(pigLatin);
+
+                string noEntry = "";
+
+                while(!string.IsNullOrEmpty(noEntry))
+                {
+                    //write lines to console from here
+                   
+                    
+
+                    Console.WriteLine("Invalid entry try again.");
+                    Console.ReadLine();
+
+                    userContinue = "";
+                    while (userContinue != "y" && userContinue != "n")
+                    {
+                        Console.WriteLine("Would you like to do another translation, Y/N ?");
+                        userContinue = Console.ReadLine().ToLower();
+
+                    }
+                }
+            }
+
+
+
+
+        }
+
+        private static string GetSentenceInPigLatin(string sentence)
+        {
+            const string vowels = "AEIOUaeio";
+            var returnSentence = "";
+            foreach (var word in sentence.Split())
+            {
+                var firstLetter = word.Substring(0, 1).ToLower();
+                var restOfWord = word.Substring(1, word.Length - 1).ToLower();
+                var currentLetter = vowels.IndexOf(firstLetter, StringComparison.Ordinal);
+
+                if (currentLetter == -1)
+                {
+                    returnSentence += restOfWord + firstLetter + "ay ";
+                }
+                else
+                {
+                    returnSentence += word + "way ";
+                }
+                
+            }
+            return returnSentence; 
+        }
+    }
+  }
